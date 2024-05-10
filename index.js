097d3d241472270238229cff5159d7c59dacd937
Nishant Maheshwari <mnishant222@gmail.com>
2024-05-11 04:19:25 +0530

    developed a Interface for CHAT APPLICATION

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 require('dotenv').config();
+const cors=require('cors');
 const app = require('express')();
 const axios = require('axios');
 const { response } = require('express');
 const PORT = process.env.NODE_PORT || 3000;
 const api_key = process.env.API_KEY;
 
+
+app.use(cors());
+
+
 app.get('/', (req, res) => {
     res.json({ message: 'Server is Stable!' });
 });
 
-app.get('/:Prompt', (req, res) => {
+app.get('/prompt/:Prompt', (req, res) => {
     const prompt = req.params.Prompt;
 
     axios.post('https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent', {
@@ -30,11 +35,7 @@ app.get('/:Prompt', (req, res) => {
     }).then((response) => {
         const data = response.data;
         const answer=data.candidates[0].content.parts[0].text;
-        res.end(`
-        <pre>
-        ${answer}
-        </pre>
-        `)
+        res.json({generated:answer});
     }).catch((error) => {
         res.json({ message: error.message });
     });
